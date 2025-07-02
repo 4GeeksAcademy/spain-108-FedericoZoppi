b3afdb3f5b6ffb827a0a33c50ce436818216aeb1
FEDERICO ZOPPI <federicozoppi.ita@gmail.com>
2025-07-02 14:48:14 +0000

    models Terminados

--- src/api/models.py
+++ src/api/models.py
@@ -6,58 +6,109 @@ db = SQLAlchemy()
 
 
 class Users(db.Model):
+    __tablename__='users'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
     first_name = db.Column(db.String, nullable = True)
     last_name = db.Column(db.String, nullable = True)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    in_admin = db.Column(db.Boolean, nullable=False)
 
-    def __repr__(self):
-        return f'<User {self.email}>'
 
     def serialize(self):
         # do not serialize the password, its a secururity breach
         return {"id": self.id,
             "email": self.email,
-            "is_active": self.is_active,
             "first_name": self.first_name,
-            "last_name": self.last_name}
+            "last_name": self.last_name,
+            "is_active": self.is_active,
+            "is_admin": self.is_admin}
     
 
+    def __repr__(self):
+        return f'<User {self.email}>'
+
+
 class Posts(db.Model):
+    __tablename__= 'posts'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable = False)
     description = db.Column(db.String, nullable = True)
     body = db.Column(db.String)
     date = db.Column(db.Date, nullable = False)
     image_url = db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeingKey('users.id'))
+    user_id = db.relationship('User', foreing_keys = [user_id],
+                                   backref=db.backref('user_id_to', lazy='select'))
     
 
 class Media(db.Model):
+    __tablename__='media'
     id = db.Column(db.Integer, primary_key=True)
     media_type = db.Column(db.Enum( "image", "video", "audio", name='media_type'), nullable=False)
     url = db.Column(db.String)
+    post_id = db.Column(db.Integer, db.ForeingKey('posts.id'))
+    post_id = db.relationship ('Post',foreing_keys = [post_id],
+                              backref = db.backref('post_id_to', lazy = 'select'))
 
 
 class Followers(db.Model):
+    __tablename__ = 'followers'
     id = db.Column(db.Integer, primary_key=True)
+    following_id = db.Column(db.Integer, db.ForeingKey('users.id'))
+    following_id = db.relationship('User', foreing_keys = [following_id],
+                                   backref=db.backref('following_to', lazy='select'))
+    follower_id = db.Column(db.Integer, db.ForeingKey('users.id'))
+    follower_id = db.relationship('User', foreing_keys = [following_id],
+                                   backref=db.backref('following_to', lazy='select'))
+
+    def serialize(self):
+        return {'id': self.id,
+                'following_id': self.following_id,
+                'follower_id': self.follower_id}
 
 
 class Comments(db.Model):
+    __tablename__ = 'comments'
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeingKey('users.id'))
+    user_id = db.relationship('User', foreing_keys = [user_id],
+                                   backref=db.backref('user_id_to', lazy='select'))
+    post_id = db.Column(db.Integer, db.ForeingKey('posts.id',))
+    post_id = db.relationship('Post', foreing_keys = [post_id],
+                                   backref=db.backref('post_id_to', lazy='select'))
+
+
+
+
+
 
 
 class CharacterFavorites(db.Model):
+    __tablename__='character_favorites'
     id = db.Column(db.Integer, primary_key=True)
-
+    user_id = db.Column(db.Integer, db.ForeingKey('users.id'))
+    user_id = db.relationship('User', foreing_keys = [user_id],
+                                   backref=db.backref('user_id_to', lazy='select'))
+    character_id = db.Column(db.Integer, db.ForeingKey('characters.id'))
+    character_id = db.relationship('Characters', foreing_keys = [character_id],
+                                   backref=db.backref('character_id_to', lazy='select'))
 
 class PlanetFavorites(db.Model):
+    __tablename__='planet_favorites'
     id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeingKey('users.id'))
+    user_id = db.relationship('User', foreing_keys = [user_id],
+                                   backref=db.backref('user_id_to', lazy='select'))
+    planet_id = db.Column(db.Integer, db.ForeingKey('planets.id'))
+    planet_id = db.relationship('Planet', foreing_keys = [planet_id],
+                                   backref=db.backref('planet_id_to', lazy='select'))
 
 
 class Characters(db.Model):
+    __tablename__='characters'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     height = db.Column(db.String)
@@ -70,6 +121,7 @@ class Characters(db.Model):
 
 
 class Planets(db.Model):
+    __nametable__='planets'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     diameter = db.Column(db.String)
@@ -79,3 +131,5 @@ class Planets(db.Model):
     population = db.Column(db.String)
     climate = db.Column(db.String)
     terrain = db.Column(db.String)
+
+
--- src/api/routes.py
+++ src/api/routes.py
@@ -4,14 +4,96 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
-from api.models import db, Users
+from api.models import db, Users, Followers
 
 
 api = Blueprint('api', __name__)
-CORS(api) # Allow CORS requests to this API
-
+CORS(api)  # Allow CORS requests to this API
 
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
     response_body = {"message": "Hello! I'm a message that came from the backend"}
     return response_body, 200
+
+
+@api.route('/users', methods=['GET', 'POST'])
+def users():
+    response_body = {}
+    if request.method == 'GET':
+        response_body['message'] = 'Respuesta del GET'
+        # ejecuando ( SELECT * FROM users )  -- scalars devuelve un iterable (similar a una lista)
+        rows = db.session.execute(db.select(Users).where(Users.is_active)).scalars()
+        # result = [row.serialize() for row in rows]
+        response_body['results'] = [row.serialize() for row in rows]  # list comprehension
+        return response_body, 200
+    if request.method == 'POST': 
+        data = request.json
+        print(data)
+        user = Users()
+        user.email = data.get('email', 'user@email.com')
+        user.password = data.get('password', '1')
+        user.is_active = True
+        user.is_admin = data.get('is_admin', False)
+        user.first_name = data.get('first_name', None)
+        user.last_name = data.get('last_name', None)
+        db.session.add(user)
+        db.session.commit()
+        response_body['results'] = user.serialize()
+        response_body['message'] = 'Respuesta del Post de Users'
+        return response_body, 201
+
+
+@api.route('/users/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+def user(id):
+    response_body = {}
+    user = db.session.execute(db.select(Users).where(Users.id == id)).scalar()
+    if not user:
+        response_body['message'] = f'Usuario {id} no encontrado'
+        response_body['results'] = None
+        return response_body, 403         
+    if request.method == 'GET':
+        response_body['message'] = f'Usuario {id} encontrado'
+        response_body['results'] = user.serialize()
+        return response_body, 200
+    if request.method == 'PUT':
+        data = request.json
+        user.email = data.get('email', user.email)
+        user.password = data.get('password', user.password)
+        # user.is_active = True
+        user.is_admin = data.get('is_admin', user.is_admin)
+        user.first_name = data.get('first_name', user.first_name)
+        user.last_name = data.get('last_name', user.last_name)
+        db.session.commit()
+        response_body['message'] = f'Usuario {id} modificado'
+        response_body['results'] = user.serialize()
+        return response_body, 200
+    if request.method == 'DELETE':
+        user.is_active = False
+        db.session.commit()
+        response_body['message'] = f'Usuario {id} eliminado'
+        response_body['results'] = None
+        return response_body, 200
+
+
+@api.route('/followers', methods=['POST'])
+def follower():
+    # Voy a recibir el token del usuario del que sigue (follower)
+    follower_id = 9
+    data = request.json
+    following_id = data.get('following_id', None)
+    response_body = {}
+    following = db.session.execute(db.select(Followers).
+                                   where(Followers.follower_id == follower_id and Followers.follower_id == following_id)).scalar()
+    if following:
+        response_body['message'] = f'El usuario {follower_id} ya es seguidor del usuario {following_id}'
+        response_body['results'] = None
+        return response_body, 403
+    if request.method == 'POST':
+        follow = Followers()
+        follow.follower_id = follower_id
+        follow.following_id = following_id
+        db.session.add(follow)
+        db.session.commit()
+        response_body['message'] = f'El usuario {follower_id} ahora segue al usuario {following_id}'
+        response_body['results'] = follow.serialize()
+        return response_body, 200
